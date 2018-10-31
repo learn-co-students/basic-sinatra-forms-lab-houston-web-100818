6b1ea0562c11b22a780b53903983fbc031fad154
Alexander Carlin <ahcarlin@gmail.com>
2018-10-31 17:09:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+get '/newteam' do 
+  erb :newteam
+end 
+
+get '/team' do 
+  erb :team
+end
+
+post '/team' do 
+  @newteam = params
+  erb :team
+  redirect '/team'
+end
 
 end
